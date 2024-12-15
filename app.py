b263555308627049129da2dcfc047e12203f052e
iVonka <hacajova.ajvonka@gmail.com>
2024-12-15 23:13:54 +0100

    download model from aws

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 import pickle
+import boto3
+import os
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+
+# Create a Boto3 S3 client
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=aws_region
+)
+
+bucket_name = os.getenv("S3_BUCKET")
+file_key = os.getenv("MODEL_NAME")
+local_file_path = "model.pkl"
+
+# Download the model file from S3
+s3.download_file(bucket_name, file_key, local_file_path)
+print(f"Model downloaded to {local_file_path}")
+
 
 # Load the saved model
 with open('model.pkl', 'rb') as file:
